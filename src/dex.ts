c4840f4f7069d8ffe5dc396c0ca15f689c2bbbb4
Aamir <usmaniaamir530@gmail.com>
2022-09-15 19:53:34 +0530

    first commit

--- src/dex.ts
+++ src/dex.ts
@@ -1,93 +1,131 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
-  DEX,
   EthToTokenSwapSuccessfull,
   LiquidityWithdrawSuccessfull,
-  OwnershipTransferred,
-  TokenAddressesSet,
   TokenToEthSwapSuccessfull,
-  TokensAdded,
-  TokensRemvod,
-  chainLinkPriceFeedAdded,
   poolCreated,
-  tokenSwappedSuccessfully
-} from "../generated/DEX/DEX"
-import { ExampleEntity } from "../generated/schema"
+  tokenSwappedSuccessfully,
+} from "../generated/DEX/DEX";
+
+import {
+  EthToTokenSwapSuccessfullEntity,
+  LiquidityWithdrawSuccessfullEntity,
+  TokenToEthSwapSuccessfullEntity,
+  poolCreatedEntity,
+  tokenSwappedSuccessfullyEntity,
+  allTransaction,
+} from "../generated/schema";
 
 export function handleEthToTokenSwapSuccessfull(
   event: EthToTokenSwapSuccessfull
 ): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let EthToToken = new EthToTokenSwapSuccessfullEntity(
+    event.transaction.hash.toHex()
+  );
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  let allTx = new allTransaction(event.transaction.hash.toHex());
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  EthToToken.ethSwapped = event.params.ethSwapped;
+  EthToToken.tokenReceived = event.params.tokenReceived;
+  EthToToken.transactionFee = event.params.transactionFee;
+  EthToToken.user = event.params.user;
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  allTx.tokenSwapped = event.params.ethSwapped;
+  allTx.tokenReceived = event.params.tokenReceived;
+  allTx.transactionFee = event.params.transactionFee;
+  allTx.user = event.params.user;
 
-  // Entity fields can be set based on event parameters
-  entity.ethSwapped = event.params.ethSwapped
-  entity.tokenReceived = event.params.tokenReceived
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.calculateExchangeToken(...)
-  // - contract.calculateExchangeTokenAfterFee(...)
-  // - contract.checkContributionIds(...)
-  // - contract.checkPoolBalanceForToken(...)
-  // - contract.checkPoolWithId(...)
-  // - contract.getPriceFeedContract(...)
-  // - contract.owner(...)
-  // - contract.s_validTokenAddresses(...)
-  // - contract.supportedTokens(...)
+  allTx.save();
+  EthToToken.save();
 }
 
 export function handleLiquidityWithdrawSuccessfull(
   event: LiquidityWithdrawSuccessfull
-): void {}
+): void {
+  let poolWithdrew = poolCreatedEntity.load(event.params.poolId.toString());
+  let removedPool = LiquidityWithdrawSuccessfullEntity.load(
+    event.params.poolId.toString()
+  );
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+  if (!removedPool) {
+    removedPool = new LiquidityWithdrawSuccessfullEntity(
+      event.params.poolId.toString()
+    );
+  }
 
-export function handleTokenAddressesSet(event: TokenAddressesSet): void {}
+  if (!poolWithdrew) {
+    poolWithdrew = new poolCreatedEntity(event.params.poolId.toString());
+  }
+
+  poolWithdrew.active = false;
+
+  removedPool.poolId = event.params.poolId;
+  removedPool.token = event.params.token;
+  removedPool.user = event.params.user;
+  removedPool.totalInterestEarned = event.params.totalInterestEarned;
+  removedPool.tokenToRecieve = event.params.tokenToRecieve;
+  removedPool.tokenToReceiveWithInterest =
+    event.params.tokenToReceiveWithInterest;
+
+  removedPool.save();
+  poolWithdrew.save();
+}
 
 export function handleTokenToEthSwapSuccessfull(
   event: TokenToEthSwapSuccessfull
-): void {}
+): void {
+  let tokenToEth = new TokenToEthSwapSuccessfullEntity(
+    event.transaction.hash.toHex()
+  );
 
-export function handleTokensAdded(event: TokensAdded): void {}
+  tokenToEth.tokenSwapped = event.params.tokenSwapped;
+  tokenToEth.tokenReceived = event.params.tokenReceived;
+  tokenToEth.transactionFee = event.params.transactionFee;
+  tokenToEth.user = event.params.user;
 
-export function handleTokensRemvod(event: TokensRemvod): void {}
+  let allTx = new allTransaction(event.transaction.hash.toHex());
+  allTx.tokenSwapped = event.params.tokenSwapped;
+  allTx.tokenReceived = event.params.tokenReceived;
+  allTx.transactionFee = event.params.transactionFee;
+  allTx.user = event.params.user;
 
-export function handlechainLinkPriceFeedAdded(
-  event: chainLinkPriceFeedAdded
-): void {}
+  allTx.save();
+  tokenToEth.save();
+}
 
-export function handlepoolCreated(event: poolCreated): void {}
+export function handlepoolCreated(event: poolCreated): void {
+  let pool = new poolCreatedEntity(event.params.poolId.toString());
+
+  pool.id = event.transaction.hash.toHex();
+  pool.active = event.params.active;
+  pool.amount = event.params.amount;
+  pool.poolId = event.params.poolId;
+  pool.owner = event.params.owner;
+  pool.token = event.params.token;
+  pool.timestamp = event.params.timeStamp;
+  pool.minLockPeriod = event.params.minLockPeriod;
+
+  pool.save();
+}
 
 export function handletokenSwappedSuccessfully(
   event: tokenSwappedSuccessfully
-): void {}
+): void {
+  let tokenToToken = new tokenSwappedSuccessfullyEntity(
+    event.transaction.hash.toHex()
+  );
+
+  tokenToToken.tokenSwapped = event.params.tokenSwapped;
+  tokenToToken.tokenReceived = event.params.tokenReceived;
+  tokenToToken.transactionFee = event.params.transactionFee;
+  tokenToToken.user = event.params.user;
+
+  let allTx = new allTransaction(event.transaction.hash.toHex());
+  allTx.tokenSwapped = event.params.tokenReceived;
+  allTx.tokenReceived = event.params.tokenReceived;
+  allTx.transactionFee = event.params.transactionFee;
+  allTx.user = event.params.user;
+
+  allTx.save();
+  tokenToToken.save();
+}
